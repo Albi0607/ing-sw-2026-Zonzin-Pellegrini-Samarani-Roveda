f8a81bec992fe1a2ec50d0f5d9c70fe2550baee3
annasamarani <anna.samarani@mail.polimi.it>
2026-03-13 20:41:52 +0100

    enumerazioni

---
+++ src/main/java/it/polimi/ingsw/Era.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw;
+
+public enum Era {
+        ERA_I,
+        ERA_II,
+        ERA_III
+    }
+
---
+++ src/main/java/it/polimi/ingsw/ResourceType.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw;
+
+public enum ResourceType {
+    FOOD,
+    PRESTIGE_POINTS
+}
