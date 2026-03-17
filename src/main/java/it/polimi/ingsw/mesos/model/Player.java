322e69b4999d2bed03e9b3ff980a8796f7b94195
Albi0607 <alberto.roveda@mail.polimi.it>
2026-03-17 14:18:21 +0100

    Primi test semplicissimi

--- src/main/java/it/polimi/ingsw/mesos/model/Player.java
+++ src/main/java/it/polimi/ingsw/mesos/model/Player.java
@@ -15,11 +15,7 @@ public class Player {
     /**
      * @return false if the player does not have enough food
      */
-    /* meglio forse che il metodo sia int cosi ritorna quanto cibo manca da pagare e cosi da sapere quanti punti
-    prestigio dover perdere di conseguenza?->alberto*/
     public int payFood(int amount) { return 0; }
-    /*meglio forse avere un altro metodo per perdere punti prestigio o averne uno unico che si chiama con un modo
-    diverso per non confondere?->alberto*/
 
     // secondo me è meglio che l'aggiornamento dei punti sia gestito da un metodo fine turno/evento che itera
     // l'update per tutti i player piuttosto che chiamare ripetutamente lo stesso metodo da istanze diverse di player
