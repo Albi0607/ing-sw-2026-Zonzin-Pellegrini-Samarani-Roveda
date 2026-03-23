d5af255a95c24acdc4a5f535fd903c76032f25ff
annasamarani <anna.samarani@mail.polimi.it>
2026-03-23 10:14:04 +0100

    aggiornamento test

--- src/test/java/it/polimi/ingsw/mesos/model/BoardTest/TurnOrderTrackTest.java
+++ src/test/java/it/polimi/ingsw/mesos/model/BoardTest/TurnOrderTrackTest.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.mesos.model.BoardTest;
 import it.polimi.ingsw.mesos.model.Player;
 import it.polimi.ingsw.mesos.model.board.Board;
 import it.polimi.ingsw.mesos.model.board.TurnOrderTrack;
+import it.polimi.ingsw.mesos.model.enums.Color;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
@@ -16,7 +17,7 @@ public class TurnOrderTrackTest {
     void testSetPlayerAt() {
         Board board = new Board(3);
         board.initializeTurnOrderTrack(3);
-        Player p = new Player("Alice", "red");
+        Player p = new Player("Alice", Color.RED);
 
         board.getTurnOrderTrack().setPlayerAt(0, p);
         assertEquals(p, board.getTurnOrderTrack().getPlayerAt(0));
@@ -26,8 +27,8 @@ public class TurnOrderTrackTest {
     void testSetPlayerAtAlreadyTaken() {
         Board board = new Board(3);
         board.initializeTurnOrderTrack(3);
-        Player p1 = new Player("Alice", "red");
-        Player p2 = new Player("Anna", "blue");
+        Player p1 = new Player("Alice", Color.RED);
+        Player p2 = new Player("Anna", Color.BLUE);
 
         board.getTurnOrderTrack().setPlayerAt(0, p1);
         assertThrows(IllegalStateException.class, () -> {
@@ -39,11 +40,52 @@ public class TurnOrderTrackTest {
     void testSetPlayerAtInvalidIndex() {
         Board board = new Board(3);
         board.initializeTurnOrderTrack(3);
-        Player p = new Player("Anna", "blue");
+        Player p = new Player("Anna", Color.BLUE);
 
         assertThrows(IndexOutOfBoundsException.class, () -> {
             board.getTurnOrderTrack().setPlayerAt(10, p);
         });
+    }
+
+    @Test
+    void testSetPlayerAtPositiveEffect() {
+        Board board = new Board(2);
+        board.initializeTurnOrderTrack(2); // slots = {1, -1}
+
+        Player p = new Player("Anna", Color.BLUE);
+
+        board.getTurnOrderTrack().setPlayerAt(0, p);
+
+        assertEquals(1, p.getFood()); // slot 0 = +1
+    }
+
+    @Test
+    void testSetPlayerAtNegativeEffectPaySuccess() {
+        Board board = new Board(2);
+        board.initializeTurnOrderTrack(2); // {1, -1}
+
+        Player p = new Player("Anna", Color.BLUE);
+        p.addFood(2);
+
+        board.getTurnOrderTrack().setPlayerAt(1, p);
+
+        assertEquals(1, p.getFood()); // 2 - 1
+        assertEquals(0, p.getPrestigePoints());
+    }
+
+    @Test
+    void testSetPlayerAtNegativeEffectNotEnoughFood() {
+        Board board = new Board(2);
+        board.initializeTurnOrderTrack(2); // {1, -1}
+
+        Player p = new Player("Anna", Color.BLUE);
+        p.addFood(0);
+        p.updatePrestige(5);
+
+        board.getTurnOrderTrack().setPlayerAt(1, p);
+
+        assertEquals(0, p.getFood()); // the player can't pay --> food remains the same
+        assertEquals(3, p.getPrestigePoints()); // the player pay in prestife points
     }
 
     @Test
@@ -53,12 +95,12 @@ public class TurnOrderTrackTest {
 
         assertEquals(0, board.getTurnOrderTrack().getFirstFreeSlot());
 
-        Player p = new Player("Alice", "red");
+        Player p = new Player("Alice", Color.RED);
         board.getTurnOrderTrack().setPlayerAt(1, p);
 
         assertEquals(0, board.getTurnOrderTrack().getFirstFreeSlot());
 
-        Player p2 = new Player("Bob", "red");
+        Player p2 = new Player("Bob", Color.RED);
         board.getTurnOrderTrack().setPlayerAt(0, p2);
 
         assertEquals(-1, board.getTurnOrderTrack().getFirstFreeSlot()); // all slot occupied
