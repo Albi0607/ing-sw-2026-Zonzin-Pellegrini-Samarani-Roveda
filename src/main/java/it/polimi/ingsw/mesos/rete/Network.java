ed23c841665f4f91c37c38780e3d057d8637ccba
Albi0607 <alberto.roveda@mail.polimi.it>
2026-04-25 15:08:21 +0200

    Aggiunta del metodo skipOnExtraDraw nell'interfaccia network geenrica e in RMI, manca in socket

--- src/main/java/it/polimi/ingsw/mesos/rete/Network.java
+++ src/main/java/it/polimi/ingsw/mesos/rete/Network.java
@@ -33,6 +33,15 @@ public interface Network {
     boolean takeCard(String nickname,int position,boolean isUpper);
 
     /**
+     * Method that allows the client not to draw the extra card at the end of the turn if they possess the triggering
+     * building
+     * @param nickname name of the player performing the action
+     * @return true if the client has chosen not to draw the extra card due to the effect of the triggering building;
+     * otherwise, false
+     */
+    boolean skipExtraDraw(String nickname);
+
+    /**
      * Method that allows the player (to be used only if they are the first connected player) to choose the number of
      * players participating in the game
      * @param numPlayers number of players selected, ranging from 2 to 5
