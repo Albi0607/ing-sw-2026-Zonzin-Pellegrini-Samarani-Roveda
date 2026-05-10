1f258a8c81bcea6e6a5e3cd2020fc47067462e9b
Mattia Zonzin <mattia.zonzin@mail.polimi.it>
2026-05-10 15:08:03 +0200

    ResolvingState, SetupState updated
    MesosIntegrationTest updated

--- src/test/java/it/polimi/ingsw/mesos/controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/mesos/controller/GameControllerTest.java
@@ -37,6 +37,8 @@ class GameControllerTest {
             @Override public String getNickname() { return "Test"; }
             @Override public void sendLobby(List<LobbyInfoDTO> lobby) {}
             @Override public String getId() {return "";}
+            @Override public void showActionRejected(String reason) {};
+            @Override public void showActionAccepted(String message) {};
         };
     }
 
@@ -296,6 +298,8 @@ class GameControllerTest {
         @Override public String getNickname() { return "fake"; }
         @Override public void sendLobby(List<LobbyInfoDTO> lobby) {}
         @Override public String getId() { return ""; }
+        @Override public void showActionRejected(String reason) {};
+        @Override public void showActionAccepted(String message) {}
 
         public List<String> getMessages() { return messages; }
     }
