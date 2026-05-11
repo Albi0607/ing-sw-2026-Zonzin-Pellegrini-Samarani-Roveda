70b9d815d7a2894d22f6c5714d854ca50c9ed03d
Albi0607 <alberto.roveda@mail.polimi.it>
2026-05-11 19:23:34 +0200

    Implementazione della GUI per la prima parte di login, lobby e waitingRoom

--- src/main/java/it/polimi/ingsw/mesos/view/GUI/Controllers/LobbyController.java
+++ src/main/java/it/polimi/ingsw/mesos/view/GUI/Controllers/LobbyController.java
@@ -1,19 +1,103 @@
 package it.polimi.ingsw.mesos.view.GUI.Controllers;
 
 import it.polimi.ingsw.mesos.rete.ClientController;
+import it.polimi.ingsw.mesos.rete.ClientModel.LobbyInfoDTO;
+import it.polimi.ingsw.mesos.view.GUI.Core.GUI;
 import it.polimi.ingsw.mesos.view.GUI.Core.ObservableGameModel;
+import it.polimi.ingsw.mesos.view.GUI.Core.SceneManager;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
+
 
 public class LobbyController {
-    private ClientController clientCtrl;
-    private ObservableGameModel gameModel;
+    private GUI gui;
+    private ClientController clientController;
+    private SceneManager sceneManager;
 
-    public void setController(ClientController clientCtrl, ObservableGameModel gameModel) { }
+    public void setController(GUI gui,ClientController clientCtrl,SceneManager sceneManager) {
+        this.gui = gui;
+        this.clientController = clientCtrl;
+        this.sceneManager = sceneManager;
+    }
+
+    @FXML private ScrollPane gamesContainer;
+    @FXML private VBox gamesVBox;
+    @FXML private ComboBox<Integer> playersComboBox;
+    @FXML private Label errorLabel;
+    @FXML private Button createGameButton;
+
+
+    @FXML public void initialize() {
+
+        //riempo il comboBox con i possibili giocatori di una partita
+        playersComboBox.getItems().addAll(2,3,4,5);
+
+        //bottone di createNewGame inizialmente disabilitato di default che si abilità se viene scelto un numero di
+        //giocatori
+        playersComboBox.valueProperty().addListener((obs,oldVal,newVal)-> {
+            createGameButton.setDisable(newVal==null);
+        });
+
+        // effetto click
+            createGameButton.setOnMousePressed(e -> {
+            createGameButton.setScaleX(0.95);
+            createGameButton.setScaleY(0.95);
+        });
+
+        createGameButton.setOnMouseReleased(e -> {
+            createGameButton.setScaleX(1.0);
+            createGameButton.setScaleY(1.0);
+        });
+    }
+
+    public void updateLobby(List<LobbyInfoDTO> lobby){
+        //pulisco l'interfaccia vecchia
+        gamesVBox.getChildren().clear();
+
+        //rifaccio e visualizzo l'interfaccia nuova
+        for (LobbyInfoDTO dto : lobby) {
+            System.out.println("CREO CARD: " + dto);
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gameCard.fxml"));
+
+                Parent card = loader.load();
+
+                GameCardController controller = loader.getController();
+                controller.setData(dto,gui,clientController,sceneManager);
+
+                gamesVBox.getChildren().add(card);
+
+            } catch (Exception e) {
+                System.out.println("ERRORE NEL CREARE LE GAMECARD: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 
     @FXML
-    public void createGame() { }
+    public void handleCreateGame() {
+        try {
+            clientController.createNewGame(playersComboBox.getValue());
+            sceneManager.loadWaitingRoom();
+            //gestire la creazione e passaggio di ID
+            //ti deve portare alla waitingRoom
+        } catch (Exception e) {
+            System.out.println("ERRORE NELLA CREAZIONE DEL GAME");
+        }
+    }
 
-    @FXML
+    //@FXML
     public void joinGame() { }
 
     public void cleanup() { }
