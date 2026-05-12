d31f53176e02ab020b9e328cda2cc6aefc28ea7e
Mattia Zonzin <mattia.zonzin@mail.polimi.it>
2026-05-12 14:07:22 +0200

    WaitingState edited

--- src/main/java/it/polimi/ingsw/mesos/view/CLI/state/WaitingState.java
+++ src/main/java/it/polimi/ingsw/mesos/view/CLI/state/WaitingState.java
@@ -4,10 +4,8 @@ import it.polimi.ingsw.mesos.view.CLI.UIContext;
 
 public class WaitingState implements UIState {
 
-    // L'unica istanza statica di questo stato
     public static final WaitingState INSTANCE = new WaitingState();
 
-    // Costruttore privato! Nessuno può fare "new WaitingState()"
     private WaitingState() {}
 
     @Override
